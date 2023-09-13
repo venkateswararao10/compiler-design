e7847020fafe3c20a630499e06c6eb367f626515
venkateswararao10 <104445024+venkateswararao10@users.noreply.github.com>
2023-09-13 12:29:08 +0530

    Update parser.py

--- level1tinyc/parser.py
+++ level1tinyc/parser.py
@@ -11,54 +11,65 @@ class parser(Parser):
         literals=lexer.literals
         memory={}
         prog=Program()
-        #mainfunc=Function()
-        #localsymbol=SymbolTable()
         @_('return_type ID "(" ")" "{" statements "}"')
         def program(self,value):
-                        self.localsymbol=SymbolTable()
-                        self.mainfunc=Function(value.ID,value.return_type,self.localsymbol)
-                        self.mainfunc.setstatementsastlist(value.statements)
-                        self.prog.addfunctiondetails(value.ID,self.mainfunc)
+                self.localsymbol=SymbolTable()
+                self.mainfunc=Function(value.return_type,value.ID,self.localsymbol)
+                self.mainfunc.setstatementsastlist(value.statements)
+                self.prog.addfunctiondetails(value.ID,self.mainfunc)
+                if self.prog.getmainfunction() == None:
+                        print('sorry,there is no main function')
+                else:
+                        return self.prog
         @_('INT')
         def return_type(self,value):
-                        pass
+                return value[0]
         @_('statement ";" statements')
         def statements(self,value):
-                        pass
+                return [value[0]] + value[2]
         @_('statement ";"')
         def statements(self,value):
-                        pass
-        @_('declaration_stmt')
+                return [value[0]]
+        @_('declaration_stmt','assisgnment_stmt','print_stmt')
         def statement(self,value):
-                        pass
-        @_('assisgnment_stmt')
-        def statement(self,value):
-                        pass
+                return value[0]
         @_('type list_of_variables')
         def declaration_stmt(self,value):
-                        pass
+                x=self.prog.getmainfunction().getlocalsymboltable()
+                for var in value[1]:
+                        if x.nameinsymboltable(var)==False:
+                                entry=SymbolTableEntry(var,value[0])
+                                self.prog.getmainfunction().getlocalsymboltable().addsymboltable(entry)
+                                self.prog.getmainfunction().getlocalsymboltable().print()
+                        else:
+                                print(f"Error: redeclaration of '{val}'")
         @_('ID "," list_of_variables')
         def list_of_variables(self,value):
-                        pass
+                x=self.prog.getmainfunction().getlocalsymboltable().getsymbolentry(value.ID)
+                return [nameAST(x)]+value[2]
         @_('ID')
         def list_of_variables(self,value):
-                 self.memory[value.ID]=0
+                x=self.prog.getmainfunction().getlocalsymboltable().getsymbolentry(value.ID)
+                return [nameAST(x)]
         @_('ID "=" ID')
         def assisgnment_stmt(self,value):
-                        self.memory[value.ID0]=self.memory[value.ID1]
+                x=self.prog.getmainfunction().getlocalsymboltable().getsymbolentry(value.ID0)
+                y=self.prog.getmainfunction().getlocalsymboltable().getsymbolentry(value.ID1)
+                left=nameASTT(x)
+                right=nameAST(y)
+                return assignAST(left,right)
         @_('ID "=" CONST')
         def assisgnment_stmt(self,value):
-                        self.memory[value.ID]=value.CONST
-        @_('print_stmt')
-        def statement(self,value):
-                        pass
+                x=self.prog.getmainfunction().getlocalsymboltable().getsymbolentry(value.ID0)
+                y=numberAST(value.CONST)
+                return assignAST(nameAST(x),y)
         @_('PRINT ID')
         def print_stmt(self,value):
-                        print(self.memory[value.ID])
-                        print(type(self.memory[value.ID]))
+                x=self.prog.getmainfunction().getlocalsymboltable().getsymbolentry(value.ID0)
+                return printAST(nameAST(x))
         @_('INT')
         def type(self,value):
-                pass
+                return value[0]
 lex=lexer()
 par=parser()
 expression='''int main(){
